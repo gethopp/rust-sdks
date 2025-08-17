5f2259311a0cf8ff5259a17f4230a01215221835
Iason Paraskevopoulos <iaswnparaskev@gmail.com>
2025-08-17 14:40:18 +0100

    Remove wa for getting sources on macos

--- webrtc-sys/include/livekit/desktop_capturer.h
+++ webrtc-sys/include/livekit/desktop_capturer.h
@@ -34,8 +34,7 @@ namespace livekit {
 class DesktopCapturer : public webrtc::DesktopCapturer::Callback {
  public:
   explicit DesktopCapturer(rust::Box<DesktopCapturerCallbackWrapper> callback,
-                           std::unique_ptr<webrtc::DesktopCapturer> capturer,
-                           std::unique_ptr<webrtc::DesktopCapturer> sources_capturer);
+                           std::unique_ptr<webrtc::DesktopCapturer> capturer);
 
   void OnCaptureResult(webrtc::DesktopCapturer::Result result,
                        std::unique_ptr<webrtc::DesktopFrame> frame) final;
@@ -48,10 +47,6 @@ class DesktopCapturer : public webrtc::DesktopCapturer::Callback {
 
  private:
   std::unique_ptr<webrtc::DesktopCapturer> capturer;
-  // TODO: This is a workaround for fetching the source list
-  // it will be removed once the SCContentSharingPicker will be
-  // integrated.
-  std::unique_ptr<webrtc::DesktopCapturer> sources_capturer;
   rust::Box<DesktopCapturerCallbackWrapper> callback;
 };
 
@@ -78,10 +73,6 @@ class DesktopFrame {
 static std::unique_ptr<DesktopCapturer> new_desktop_capturer(
     rust::Box<DesktopCapturerCallbackWrapper> callback,
     bool window_capturer) {
-  // We have a separate capturer for the sources because
-  // at the time of the implementation the screencapture kit
-  // implementation had issues.
-  // We need to fix this.
   webrtc::DesktopCaptureOptions options =
       webrtc::DesktopCaptureOptions::CreateDefault();
 #ifdef __APPLE__
@@ -92,36 +83,16 @@ static std::unique_ptr<DesktopCapturer> new_desktop_capturer(
   options.set_allow_directx_capturer(true);
 #endif
   std::unique_ptr<webrtc::DesktopCapturer> capturer = nullptr;
-  std::unique_ptr<webrtc::DesktopCapturer> sources_capturer = nullptr;
   if (window_capturer) {
     capturer = webrtc::DesktopCapturer::CreateWindowCapturer(options);
-#ifdef __APPLE__
-    options.set_allow_sck_capturer(false);
-#endif
-#ifdef _WIN64
-    // Only one can wgc screen capturer can be instantiated, according to
-    // https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/modules/desktop_capture/win/wgc_capturer_win.h;l=80.
-    options.set_allow_wgc_screen_capturer(false);
-#endif
-    sources_capturer = webrtc::DesktopCapturer::CreateWindowCapturer(options);
   } else {
     capturer = webrtc::DesktopCapturer::CreateScreenCapturer(options);
-#ifdef __APPLE__
-    options.set_allow_sck_capturer(false);
-#endif
-#ifdef _WIN64
-    // Only one can wgc screen capturer can be instantiated, according to
-    // https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/modules/desktop_capture/win/wgc_capturer_win.h;l=80.
-    options.set_allow_wgc_screen_capturer(false);
-#endif
-    sources_capturer = webrtc::DesktopCapturer::CreateScreenCapturer(options);
   }
-  if (!capturer || !sources_capturer) {
+  if (!capturer) {
     return nullptr;
   }
   return std::make_unique<DesktopCapturer>(std::move(callback),
-                                           std::move(capturer),
-                                           std::move(sources_capturer));
+                                           std::move(capturer));
 }
 
 }  // namespace livekit
